a6f61dfef8b943d6a25b00f1afdff08f0b12db44
joergbeigang <joerg@goob.de>
2025-12-16 11:04:49 +0100

    licese info added

--- grblHal_pendant/joystick.ino
+++ grblHal_pendant/joystick.ino
@@ -1,4 +1,21 @@
 /*
+This file is part of grblHal_pendant.
+grblMachinist is free software: you can redistribute it and/or modify it under the terms
+of the GNU General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version.
+
+grblMachinist is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+grblMachinist.
+If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/*
  * functions for jogging using a joystick
  * and the rotary encoder for the z axis at the same time
  *
